590c11e50c716821f007d0373c028808a98d12c5
poonam <gt.poonam@gmail.com>
2024-12-23 08:38:44 +0000

    calculating sum of string numbers

--- lib/spec/string_calculator_spec.rb
+++ lib/spec/string_calculator_spec.rb
@@ -2,10 +2,19 @@ require './lib/string_calculator.rb'
 
 describe StringCalculator do 
   let(:calculator) { StringCalculator.new }
-  context "Given two numbers" do 
+  describe "#add" do
+    it "returns 0 when the input is an empty string" do
+      expect(calculator.add("")).to eq(0)
+    end
+
     it "adds the numbers using the add method" do
-      sum = calculator.add(2,3)
-      expect(sum).to eql(5)
+      sum = calculator.add("1")
+      expect(sum).to eql(1)
+    end
+
+    it "adds the numbers using the add method" do
+      sum = calculator.add("1,6")
+      expect(sum).to eql(7)
     end
   end
 end
--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,19 @@
 class StringCalculator
 
-  def add(num1, num2)
-  	num1+num2
+  def add(numbers)
+
+    # Return 0 if the string is empty
+    return 0 if numbers.empty?
+
+    delimiter = ','
+
+    # Replace newline characters and custom delimiters
+    numbers = numbers.gsub("\n", delimiter)
+    numbers = numbers.split(delimiter)
+
+    # Return the sum of all numbers
+    numbers.map(&:to_i).sum
+
   end	
 
 end	
