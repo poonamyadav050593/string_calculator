1ec8513adb0551ad39deb215daca52e42aca9e93
poonam <gt.poonam@gmail.com>
2024-12-23 08:51:16 +0000

    handling custom delimiter

--- lib/spec/string_calculator_spec.rb
+++ lib/spec/string_calculator_spec.rb
@@ -16,5 +16,24 @@ describe StringCalculator do
       sum = calculator.add("1,6")
       expect(sum).to eql(7)
     end
+
+    it "returns the sum of multiple numbers" do
+      sum = calculator.add("1,5,6")
+      expect(sum).to eql(12)
+    end
+
+    it "returns sum of numbers with new line" do 
+      sum = calculator.add("1\n2")
+      expect(sum).to eql(3)
+    end
+    
+    it "returns sum of numbers with new lines and comma" do
+      sum = calculator.add("1\n2,4")
+      expect(sum).to eql(7)
+    end
+    
+    it "allows a custom delimiter to be used" do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end  
   end
 end
--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,7 +5,14 @@ class StringCalculator
     # Return 0 if the string is empty
     return 0 if numbers.empty?
 
-    delimiter = ','
+     # Handle custom delimiters
+    if numbers.start_with?("//")
+      delimiter_end_index = numbers.index("\n")
+      delimiter = numbers[2..delimiter_end_index-1]
+      numbers = numbers[delimiter_end_index+1..-1]
+    else
+      delimiter = ","
+    end
 
     # Replace newline characters and custom delimiters
     numbers = numbers.gsub("\n", delimiter)
